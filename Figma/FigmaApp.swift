a158235269779f45d8d702025eccf3ddb9456fd2
makram-skaff <71062663+HassanSheaib@users.noreply.github.com>
2022-02-03 23:37:12 +0200

    Initial Commit

---
+++ Figma/FigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaApp.swift
+//  Figma
+//
+//  Created by HHS on 03/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
